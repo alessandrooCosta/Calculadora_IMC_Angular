56dccfb5cd249e26d85c0c35d82f2f628b120d6a
costaalessandro-ux <75959516+costaalessandro-ux@users.noreply.github.com>
2022-10-18 15:51:34 -0300

    Update formcaixa.component.ts

--- src/app/formcaixa/formcaixa.component.ts
+++ src/app/formcaixa/formcaixa.component.ts
@@ -10,13 +10,22 @@ export class FormcaixaComponent implements OnInit {
   peso : any; 
   altura: any;
   res: any;
-
-
+  
+  calcImc(): number {
+    let imc = 0;
+    let gen = this.genero;
+    let peso = this.peso;
+    let alt = this.altura;
+    this.imc = peso / (altura * 2);
+    alert(this.imc);
+    
+    return this.imc;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
-    this.res = this.peso;
+  
   }
 
 }
