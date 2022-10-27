a928431de6a3ee7d5a7048359d2cbdc607f363a6
Alessandro <alessandrocosta103@gmail.com>
2022-10-27 16:43:38 -0300

    upgrade

--- src/app/formcaixa/formcaixa.component.ts
+++ src/app/formcaixa/formcaixa.component.ts
@@ -26,28 +26,36 @@ export class FormcaixaComponent implements OnInit {
     let calc = peso / (alt * 2);
     this.res = res;
     this.imc = calc.toFixed(1);
-    if (this.imc < 30.0) {
-      this.textRed = true;
-    } else {
-      this.textBlue = true;
-    }
     return this.imc;
   }
 
 
   get(): "#87CEEB" | "#3CB371" | "#FFFF00" | "#FFA500" | "#FF4500" | "#FF0000" | any{
-    if (this.imc < 18.5) {
-      return '#87CEEB'
-    } else if (this.imc == 18.5 || this.imc < 24.9) {
-      return '#3CB371'
-    } else if (this.imc == 25.0 || this.imc < 29.9) {
-      return "#FFFF00"
-    } else if (this.imc == 30.0 || this.imc < 34.9) {
-      return "#FFA500"
-    } else if (this.imc == 35.0 || this.imc < 39.9) {
-      return "#FF4500"
-    } else if (this.imc >= 40.0) {
-      return "#FF0000"
+    if(this.idade == 'idade1'){
+      if (this.imc < 18.5) {
+        return '#87CEEB'
+      } else if (this.imc == 18.5 || this.imc < 24.9) {
+        return '#3CB371'
+      } else if (this.imc == 25.0 || this.imc < 29.9) {
+        return "#FFFF00"
+      } else if (this.imc == 30.0 || this.imc < 34.9) {
+        return "#FFA500"
+      } else if (this.imc == 35.0 || this.imc < 39.9) {
+        return "#FF4500"
+      } else if (this.imc >= 40.0) {
+        return "#FF0000"
+      }
+    }else{
+      if (this.imc <= 22.0) {
+        //return "#FFFF00"
+        return "blue"
+    } else if (this.imc > 22.0 && this.imc < 27.0) {
+      //return "#FFA500"
+      return "violet"
+    } else if (this.imc > 27.0) {
+      //return "#FF0000"
+      return "brown"
+    }
     }
   }
 
